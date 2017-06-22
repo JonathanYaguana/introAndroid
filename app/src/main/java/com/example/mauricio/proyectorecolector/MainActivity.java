1134b2f4443d4276b75cd12a732918c0ab354c59
JonathanYaguana <jonathansacrox93@gmail.com>
2017-06-21 21:13:53 -0500

    segundo commit

--- app/src/main/java/com/example/mauricio/proyectorecolector/MainActivity.java
+++ app/src/main/java/com/example/mauricio/proyectorecolector/MainActivity.java
@@ -1,13 +1,59 @@
 package com.example.mauricio.proyectorecolector;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn_nuevo, btn_sincronizar, btn_listar, btn_camara;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_nuevo=(Button) findViewById(R.id.bt1);
+        btn_nuevo.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent
+                        (MainActivity.this,Nuevo.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_sincronizar=(Button) findViewById(R.id.bt2);
+        btn_sincronizar.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent
+                        (MainActivity.this,Sincronizar.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_listar=(Button) findViewById(R.id.bt3);
+        btn_listar.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent
+                        (MainActivity.this,Listar.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_camara=(Button) findViewById(R.id.bt4);
+        btn_camara.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent
+                        (MainActivity.this,camara.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/mauricio/proyectorecolector/Sincronizar.java
@@ -0,0 +1,32 @@
+package com.example.mauricio.proyectorecolector;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class Sincronizar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sincronizar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /*Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();*/
+                Intent intent =new Intent(Sincronizar.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
